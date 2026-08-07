4a092fe77e2e23e348b7f07d9a947767b2092d55
tere <kuresa_afamasaga@yahoo.com>
2026-08-07 14:36:52 +1000

    Deploy seek and instrumental continuity pass

--- docs/app-events.js
+++ docs/app-events.js
@@ -1,6 +1,6 @@
 'use strict';
 (()=>{
-  const BUILD='p1-20260807-2';
+  const BUILD='p1-20260807-3';
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=`${src}?v=${BUILD}`;s.onload=resolve;s.onerror=()=>reject(new Error(`Failed to load ${src}`));document.body.append(s)});
   for(const href of ['apple-motion.css','ui-solid.css','intro-layout.css']){const css=document.createElement('link');css.rel='stylesheet';css.href=`${href}?v=${BUILD}`;document.head.append(css)}
   (async()=>{
