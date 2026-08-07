0c805b051e2afc54e63e565ffcf217fa9114d672
tere <kuresa_afamasaga@yahoo.com>
2026-08-07 14:09:31 +1000

    Bump runtime for Apple motion priority pass

--- docs/app-events.js
+++ docs/app-events.js
@@ -1,6 +1,6 @@
 'use strict';
 (()=>{
-  const BUILD='p0-20260807-5';
+  const BUILD='p1-20260807-1';
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=`${src}?v=${BUILD}`;s.onload=resolve;s.onerror=()=>reject(new Error(`Failed to load ${src}`));document.body.append(s)});
   for(const href of ['apple-motion.css','ui-solid.css','intro-layout.css']){const css=document.createElement('link');css.rel='stylesheet';css.href=`${href}?v=${BUILD}`;document.head.append(css)}
   (async()=>{
