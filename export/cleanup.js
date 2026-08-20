1ff6b15011fef354290f0e17f837b4f11b0d476e
tere <kuresa_afamasaga@yahoo.com>
2026-08-20 17:37:36 +1000

    Remove obsolete exporter cleanup module

--- export/cleanup.js
+++
@@ -1,9 +0,0 @@
-import { releaseEncoder } from './encoder.js';
-
-export async function cleanupEncoder(encoder, files = []) {
-    if (!encoder) return;
-    for (const file of files) {
-        try { await encoder.deleteFile(file); } catch {}
-    }
-    releaseEncoder(encoder);
-}
